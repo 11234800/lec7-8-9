9f02dd89c6800ab4ae50ca6dbb592280d88bfffc
Zeyad Baleigh <zeyadbaleigh@gmail.com>
2026-07-26 07:34:55 +0300

    Add spring framework tasks

---
+++ lec8/src/main/java/com/spring/boot910/lec8/Lec8Application.java
@@ -0,0 +1,13 @@
+package com.spring.boot910.lec8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lec8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lec8Application.class, args);
+    }
+
+}
---
+++ lec8/src/test/java/com/spring/boot910/lec8/Lec8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.boot910.lec8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lec8ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
