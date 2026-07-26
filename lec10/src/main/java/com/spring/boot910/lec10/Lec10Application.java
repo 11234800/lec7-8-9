3f4bc1946deeec1e788ecc80711da6705ca36e19
Zeyad Baleigh <zeyadbaleigh@gmail.com>
2026-07-26 07:50:20 +0300

    update

---
+++ lec10/src/main/java/com/spring/boot910/lec10/Lec10Application.java
@@ -0,0 +1,13 @@
+package com.spring.boot910.lec10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lec10Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lec10Application.class, args);
+    }
+
+}
---
+++ lec10/src/test/java/com/spring/boot910/lec10/Lec10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.boot910.lec10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lec10ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
