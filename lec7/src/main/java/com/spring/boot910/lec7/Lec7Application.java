9e9f7e3a69f941467e4406e27a230e2744a3e419
Zeyad Baleigh <zeyadbaleigh@gmail.com>
2026-07-26 07:55:20 +0300

    .

--- lec7/src/main/java/com/spring/boot910/lec7/Lec7Application.java
+++ lec7/src/main/java/com/spring/boot910/lec7/Lec7Application.java
@@ -2,12 +2,13 @@ package com.spring.boot910.lec7;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.converter.json.GsonBuilderUtils;
 
 @SpringBootApplication
 public class Lec7Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Lec7Application.class, args);
+        System.out.println("Lec7Application <UNK>");
     }
-
 }
