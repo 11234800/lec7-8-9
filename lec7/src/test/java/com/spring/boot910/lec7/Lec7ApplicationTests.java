65f8738dcd1159be8b5aed0ed20a77d5885014ea
Zeyad Baleigh <zeyadbaleigh@gmail.com>
2026-07-26 07:31:05 +0300

    Add spring framework tasks

---
+++ lec7/src/test/java/com/spring/boot910/lec7/Lec7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.boot910.lec7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lec7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
